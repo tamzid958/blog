7f6e7675f1e1c4c55b06673cf6ee1d644939a315
mrtvirus <mrtvirus@kali.mrtvirushub.com>
2020-10-16 16:41:59 +0600

    major fix

--- includes/footer.php
+++ includes/footer.php
@@ -4,15 +4,15 @@
             <p class="text-center">Copyright &#169; by Tamzid Ahmed </p>
         </div>
     </div>
-    <script type="text/javascript" src="/scripts/jquery.js"></script>
-    <script type="text/javascript" src="/scripts/popper.min.js"></script>
-    <script type="text/javascript" src="/scripts/bootstrap.bundle.min.js"></script>
-    <script type="text/javascript" src="/scripts/chart.bundle.js"></script>
-    <script type="text/javascript" src="/scripts/fontawesome.min.js"></script>
+    <script type="text/javascript" src="https://<?php echo $_SERVER['SERVER_NAME'] ?>/scripts/jquery.js"></script>
+    <script type="text/javascript" src="https://<?php echo $_SERVER['SERVER_NAME'] ?>/scripts/popper.min.js"></script>
+    <script type="text/javascript" src="https://<?php echo $_SERVER['SERVER_NAME'] ?>/scripts/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="https://<?php echo $_SERVER['SERVER_NAME'] ?>/scripts/chart.bundle.js"></script>
+    <script type="text/javascript" src="https://<?php echo $_SERVER['SERVER_NAME'] ?>/scripts/fontawesome.min.js"></script>
     <script src="https://cdn.tiny.cloud/1/e4f32vgxciwftxmzhe11ch7ruh2i0wyw6jrb0qh5v986h2xr/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5f6a2229f290dcba"></script>
-    <script type="text/javascript" src="/scripts/hashes.min.js"></script>
-    <script type="text/javascript" src="/scripts/app.js"></script>
+    <script type="text/javascript" src="https://<?php echo $_SERVER['SERVER_NAME'] ?>/scripts/hashes.min.js"></script>
+    <script type="text/javascript" src="https://<?php echo $_SERVER['SERVER_NAME'] ?>/scripts/app.js"></script>
 </footer>
 </body>
 
