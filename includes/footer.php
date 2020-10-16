b671379db050060fc5c5a68702b67c2ab46d9e2c
mrtvirus <mrtvirus@kali.mrtvirushub.com>
2020-10-16 17:37:07 +0600

    defer parsing

--- includes/footer.php
+++ includes/footer.php
@@ -11,16 +11,16 @@
     <script type="text/javascript" src="/scripts/chart.bundle.js"></script>
     <script type="text/javascript" src="/scripts/fontawesome.min.js"></script>
     <script type="text/javascript" src="/scripts/hashes.min.js"></script>
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5f6a2229f290dcba"></script>
+    <script src="https://cdn.tiny.cloud/1/e4f32vgxciwftxmzhe11ch7ruh2i0wyw6jrb0qh5v986h2xr/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <script type="text/javascript" src="/scripts/app.js"></script>
 -->
-    <script src="https://cdn.tiny.cloud/1/e4f32vgxciwftxmzhe11ch7ruh2i0wyw6jrb0qh5v986h2xr/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5f6a2229f290dcba"></script>
 
 </footer>
 
 <script type="text/javascript">
     function parseJSAtOnload() {
-        var links = ["/scripts/jquery.js", "/scripts/popper.min.js", "/scripts/bootstrap.bundle.min.js", "/scripts/chart.bundle.js", "/scripts/fontawesome.min.js", "/scripts/hashes.min.js", "/scripts/app.js"],
+        var links = ["/scripts/jquery.js", "/scripts/popper.min.js", "/scripts/bootstrap.bundle.min.js", "/scripts/chart.bundle.js", "/scripts/fontawesome.min.js", "/scripts/hashes.min.js", "//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5f6a2229f290dcba", "https://cdn.tiny.cloud/1/e4f32vgxciwftxmzhe11ch7ruh2i0wyw6jrb0qh5v986h2xr/tinymce/5/tinymce.min.js", "/scripts/app.js"],
             headElement = document.getElementsByTagName("head")[0],
             linkElement, i;
         for (i = 0; i < links.length; i++) {
