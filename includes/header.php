bf25799281dccbaa0cb7545fa3b05112c7f2fb85
mrtvirus <mrtvirus@kali.mrtvirushub.com>
2020-10-19 23:42:43 +0600

    font fixed

--- includes/header.php
+++ includes/header.php
@@ -29,7 +29,7 @@ $postcount =  postcount();
     <!-- <link href="/fonts/roboto.min.css?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <link href="/fonts/opensans.min.css?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
       <link href="/fonts/dosisfont.min.css?family=Dosis:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet"> -->
-    <link rel="stylesheet" type="text/css" href="/fonts/oliwe.css">
+    <link rel="stylesheet" type="text/css" href="/fonts/font.css">
     <link rel="stylesheet" type="text/css" href="/css/chart.min.css" />
     <link rel="shortcut icon" href="/images/logo/<?php echo $site_details[0]["site_favicon"] ?>" type="image/x-icon">
     <link rel="icon" href="/images/logo/<?php echo $site_details[0]["site_favicon"] ?>" type="image/x-icon">
