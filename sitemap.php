f8c5abdcffca3c650fc8264a8a71a0055d59d8e4
mrtvirus <mrtvirus@kali.mrtvirushub.com>
2020-10-21 11:28:21 +0600

    sitemap

--- sitemap.php
+++ sitemap.php
@@ -9,7 +9,7 @@ header("Content-Type: application/xml; charset=utf-8");
 
 echo '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
 
-echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" ' . PHP_EOL;
+echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL;
 
 foreach ($sitemap_urls as $sitemap_url) {
     echo '<url>' . PHP_EOL;
