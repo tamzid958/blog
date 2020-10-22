032a5df88a6e2afa4ae58198151b9b60016af080
mrtvirus <mrtvirus@kali.mrtvirushub.com>
2020-10-22 23:07:03 +0600

    fixed

--- scripts/app.js
+++ scripts/app.js
@@ -4,10 +4,8 @@ $(document).ready(function () {
   });
   tinymce.init({
     selector: ".admin_textarea",
-    plugins:
-      "a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker",
-    toolbar:
-      "a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table",
+    plugins: 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+    toolbar_mode: 'floating',
     toolbar_mode: "floating",
     tinycomments_mode: "embedded",
     tinycomments_author: "Author name",
