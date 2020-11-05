243ed4378cf029f25366f718efc5f9837a515fda
mrtvirus <tamjidahmed958@gmail.com>
2020-11-06 01:15:22 +0600

    tes

--- includes/meta_header.php
+++ includes/meta_header.php
@@ -5,6 +5,6 @@
 <meta property='article:publisher' content='https://technologea.com/' />
 <meta property="og:title" content="Blog | Tamzid Ahmed">
 <meta property="og:description" content="Tired warrior I have learned to fall and stand again and again Yet I did not learn to lose">
-<meta property="og:image:url" content="https://blog.mrtvirushub.com/images/logo/banner.png">
+<meta property="og:image" content="https://blog.mrtvirushub.com/images/logo/banner.png">
 <meta property="og:url" content="<?php echo $_SERVER['REQUEST_URI'] ?>">
 <meta name="twitter:card" content="summary">
