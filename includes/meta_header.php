221075a6d5aa79fbf61c1e4354af347bc983665e
mrtvirus <tamjidahmed958@gmail.com>
2020-11-06 00:14:52 +0600

    push

--- includes/meta_header.php
+++ includes/meta_header.php
@@ -3,7 +3,7 @@
 <meta property="og:type" content="article">
 <meta property='article:author' content='https://www.linkedin.com/in/tamzid-ahmed958/' />
 <meta property='article:publisher' content='https://technologea.com/' />
-<meta property="og:title" content="Blog of Tamzid Ahmed">
+<meta property="og:title" content="Blog | Tamzid Ahmed">
 <meta property="og:description" content="Tired warrior I have learned to fall and stand again and again Yet I did not learn to lose">
 <meta property="og:image:url" content="https://<?php echo $_SERVER['SERVER_NAME'] ?>/images/logo/banner.png">
 <meta property="og:url" content="<?php echo $_SERVER['REQUEST_URI'] ?>">
