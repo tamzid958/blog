baa47a6b7190009dd7f37aea813747e9ff401141
mrtvirus <tamjidahmed958@gmail.com>
2020-11-06 14:45:47 +0600

    asd

--- post.php
+++ post.php
@@ -76,7 +76,7 @@ if (getPost($_REQUEST["url"])) {
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4 d-sm-none d-md-block d-lg-block">
+                <div class="col-md-4">
                     <br>
                     <ul class="list-group list-group-flush text-dark">
                         <div class="card card-inner">
