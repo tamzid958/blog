d8d3e0ca9a1d43886c093985dd2d5f45a640e2f6
tamzid958 <tamjidahmed958@gmail.com>
2020-11-28 22:55:53 +0600

    drop database if error occured

--- controller/database_model.php
+++ controller/database_model.php
@@ -1,7 +1,13 @@
 <?php
 include 'config.inc.php';
 
+$mysqli = new mysqli($servername, $db_username, $db_password);
+
+$check_database_exist_or_not = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$db_name'";
+
 $db_create = "CREATE DATABASE $db_name; ";
+$db_remove = "DROP DATABASE $db_name; ";
+
 $init_sqls = array(
     "CREATE TABLE author (`id` int(1) NOT NULL,`author_name` varchar(50) NOT NULL,`author_tel` varchar(50) NOT NULL,`author_mail` varchar(50) NOT NULL,`password` varchar(100) NOT NULL,`biography` text NOT NULL,`adsense_code` mediumtext NOT NULL,`site_name` varchar(10) NOT NULL,`site_logo` varchar(100) NOT NULL,`author_img` varchar(100) NOT NULL,`site_favicon` varchar(40) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;",
     "CREATE TABLE `category` (`category_id` int(100) NOT NULL,`category_name` varchar(100) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4; ",
@@ -20,23 +26,24 @@ $init_sqls = array(
 );
 
 
-$mysqli = new mysqli($servername, $db_username, $db_password);
-
-$check_database_exist_or_not = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$db_name'";
-
 if ($mysqli === false) {
     echo "no server connection";
     exit;
 }
 
 if (getArrayforDatabaseCheck($check_database_exist_or_not) == null) {
-    $booting_message = "Default Login Details: \\nmail: admin@example.com \\npass: admin";
-    executeforfirsttime($db_create);
-    foreach ($init_sqls as $init_sql) {
-        execute($init_sql);
+    try {
+        $booting_message = "Default Login Details: \\nmail: admin@example.com \\npass: admin";
+        executeforfirsttime($db_create);
+        foreach ($init_sqls as $init_sql) {
+            execute($init_sql);
+        }
+        echo "<script type='text/javascript'> alert('$booting_message'); window.location.href = '/login.php'; </script>";
+        $booting_message = null;
+    } catch (Exception $e) {
+        executeforfirsttime($db_remove);
+        echo "Something went wrong: " . $e->getMessage();
     }
-    echo "<script type='text/javascript'> alert('$booting_message'); window.location.href = '/login.php'; </script>";
-    $booting_message = null;
 }
 
 function getArrayforDatabaseCheck($check_database_exist_or_not)
